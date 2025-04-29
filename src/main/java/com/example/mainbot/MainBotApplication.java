f38517c9f4721427fdfd28f946748548adce9af7
Samir <sharipovsamir480@gmail.com>
2025-04-29 16:23:42 +0500

    second commit

---
+++ src/main/java/com/example/mainbot/MainBotApplication.java
@@ -0,0 +1,12 @@
+
+package com.example.mainbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainBotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainBotApplication.class, args);
+    }
+}
